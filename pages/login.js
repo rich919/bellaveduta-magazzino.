40fe9078d2d6cb0e4a4341402f325f3c66790540
rich919 <michelericco95@gmail.com>
2025-06-21 08:34:08 +0200

    Update login.js
    Modificato campo login in text per usare username

--- pages/login.js
+++ pages/login.js
@@ -17,7 +17,12 @@ export default function Login() {
       <div className="flex items-center justify-center h-screen">
         <form onSubmit={handleLogin} className="bg-white p-6 rounded shadow">
           <h2 className="text-2xl mb-4">Admin Login</h2>
-          <input type="email" placeholder="Email" className="border p-2 w-full mb-2" onChange={e=>setEmail(e.target.value)}/>
+         <input
++            type="text"
++            placeholder="Username"
++            className="border p-2 w-full mb-2"
++            onChange={e => setEmail(e.target.value)}
++          />
           <input type="password" placeholder="Password" className="border p-2 w-full mb-4" onChange={e=>setPassword(e.target.value)}/>
           <button type="submit" className="w-full bg-blue-600 text-white p-2">Login</button>
         </form>
